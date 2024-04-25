8d088252aab29a1a4940378feb6ababd323fa735
Vitória Thaís <vitoriathdasilva@gmail.com>
2024-04-24 21:40:03 -0300

    add

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,7 @@ import express, { Request, Response } from 'express'
   app.get('/', (_req: Request, res: Response) => {
     return res.send('Express Typescript on Vercel')
   })
+  
 
   app.get('/ping', (_req: Request, res: Response) => {
     return res.send('pong 🏓')
