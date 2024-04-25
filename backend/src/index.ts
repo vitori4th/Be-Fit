57cf4b2fc4826f687c4e7aa1db77769ea9351e31
Vitória Thaís <vitoriathdasilva@gmail.com>
2024-04-24 21:54:50 -0300

    add backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,35 @@
-import express, { Request, Response } from 'express'
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db";
+import { notFound, errorHandler } from "./middlewares/ErrorMiddleware";
+import AuthRoutes from "./routes/AuthRoutes";
 
-  const app = express()
-  const port = process.env.PORT || 8080
+const app: Application = express();
 
-  app.get('/', (_req: Request, res: Response) => {
-    return res.send('Express Typescript on Vercel')
-  })
-  
+dotenv.config();
 
-  app.get('/ping', (_req: Request, res: Response) => {
-    return res.send('pong 🏓')
-  })
+connectDB();
 
-  app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`)
-  })
+app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors({
+  origin: "*",
+}));
+
+// Default
+app.get("/api", (req: Request, res: Response) => {
+  res.status(201).json({ message: "Welcome to Auth ts" });
+});
+
+// User Route
+app.use("/api/auth", AuthRoutes);
+
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (): void => console.log(`Server is running on ${PORT}`));
