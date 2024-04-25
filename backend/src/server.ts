e43ae3ea414164ae6919c08143076919500df226
Vitória Thaís <vitoriathdasilva@gmail.com>
2024-04-24 21:08:13 -0300

    add express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+
+app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
