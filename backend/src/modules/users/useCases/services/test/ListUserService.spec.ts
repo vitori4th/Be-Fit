6db5ac8ea1a3acc27cacc06baa4d982e5df8fe0f
andre.lima <andre.lima@b2ml.com.br>
2024-04-29 18:09:38 -0300

    fix: test unit

--- backend/src/modules/users/useCases/services/test/ListUserService.spec.ts
+++ backend/src/modules/users/useCases/services/test/ListUserService.spec.ts
@@ -1,58 +1,58 @@
-// import 'reflect-metadata';
-// import FakeUserRepository from '@modules/users/repositories/user/fakes/FakeUserRepository';
-// import CreateUserService from '../CreateUserService';
-// import request from 'supertest';
-// import CreateSessionsService from '../CreateSessionService';
+import 'reflect-metadata';
+import FakeUserRepository from '@modules/users/repositories/user/fakes/FakeUserRepository';
+import CreateUserService from '../CreateUserService';
+import request from 'supertest';
+import CreateSessionsService from '../CreateSessionService';
 
 
-// let userRepository: FakeUserRepository;
-// let _createUser: CreateUserService;
+let userRepository: FakeUserRepository;
+let _createUser: CreateUserService;
 
-// describe('GetUser', () => {
-//   beforeEach(() => {
-//     userRepository = new FakeUserRepository();
-//     _createUser = new CreateUserService(userRepository);
-//   });
-//   it('GET', async () => {
-//     const id = 'b4225ebe-0ab5-4a48-9615-0ba6d750d5a2';
-//     const email = 'teste@email.com';
-//     const password = 'teste';
+describe('GetUser', () => {
+  beforeEach(() => {
+    userRepository = new FakeUserRepository();
+    _createUser = new CreateUserService(userRepository);
+  });
+  it('GET', async () => {
+    const id = 'b4225ebe-0ab5-4a48-9615-0ba6d750d5a2'; // need to a id exists in DB
+    const email = 'teste@email.com';
+    const password = 'teste';
 
 
-//     const sessionService = new CreateSessionsService();
-//     const { token } = await sessionService.execute({
-//       email,
-//       password,
-//     })
+    const sessionService = new CreateSessionsService();
+    const { token } = await sessionService.execute({
+      email,
+      password,
+    })
 
-//     const response = await request('http://localhost:3333')
-//       .get(`/users/${id}`)
-//       .set('Authorization', `bearer ${token}`);
+    const response = await request('http://localhost:3333')
+      .get(`/users/${id}`)
+      .set('Authorization', `bearer ${token}`);
 
-//     expect(response.status).toBe(200);
+    expect(response.status).toBe(200);
 
-//     expect(response.body).toEqual(
-//       expect.objectContaining({
-//         cellphone: expect.any(String),
-//         cpf: expect.any(Number),
-//         createdAt: expect.any(String),
-//         dateBirth: expect.any(String),
-//         email: expect.any(String),
-//         id: expect.any(String),
-//         lastname: expect.any(String),
-//         name: expect.any(String),
-//         role: expect.any(String),
-//         updatedAt: expect.any(String),
-//       }),
-//     );
-//   });
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        cellphone: expect.any(String),
+        cpf: expect.any(Number),
+        createdAt: expect.any(String),
+        dateBirth: expect.any(String),
+        email: expect.any(String),
+        id: expect.any(String),
+        lastname: expect.any(String),
+        name: expect.any(String),
+        role: expect.any(String),
+        updatedAt: expect.any(String),
+      }),
+    );
+  });
 
-//   it('GET if id not exists, throw error', async () => {
-//     const id = 'b4225ebe-0ab5-4a48-9615-0ba6d750d5a1';
+  it('GET if id not exists, throw error', async () => {
+    const id = 'b4225ebe-0ab5-4a48-9615-0ba6d750d5a1';
 
-//     const response = await request('http://localhost:3333').get(`/users/${id}`);
+    const response = await request('http://localhost:3333').get(`/users/${id}`);
 
-//     expect(response.status).toBe(400);
-//     expect(response.body.message).toEqual('JWT Token is missing.');
-//   });
-// });
+    expect(response.status).toBe(400);
+    expect(response.body.message).toEqual('JWT Token is missing.');
+  });
+});
--- backend/src/modules/users/useCases/services/test/UpdateProfileService.spec.ts
+++ backend/src/modules/users/useCases/services/test/UpdateProfileService.spec.ts
@@ -1,69 +1,41 @@
 import request from 'supertest';
 import CreateUserService from '../CreateUserService';
 import UserRepository from '../../../repositories/user/UserRepository';
-import { prismaClient } from '../../../../../prisma/prismaClient';
 import { IUserDTO } from '../../../entities/user';
 import CreateSessionsService from '../CreateSessionService';
 
 let userRepository: UserRepository;
-let createUser: CreateUserService;
+let _createUser: CreateUserService;
 
 describe('User API', () => {
 
-  //let deleteId!: string;
-
   beforeEach(() => {
         userRepository = new UserRepository();
-        createUser = new CreateUserService(userRepository);
+        _createUser = new CreateUserService(userRepository);
       })
 
-  // afterAll(async () => {
-  //   // Limpar dados de teste após a execução dos testes
-  //   await prismaClient.user.delete({where: {id: deleteId}});
-  //   await prismaClient.$disconnect();
-  // });
-
   it('should update user information', async () => {
-    // const user =  await createUser.execute({
-    //   cellphone: '11256',
-    //   cpf: 123456,
-    //   email: 'testeee@email.com',
-    //   dateBirth: new Date('2002-12-03'),
-    //   lastname: 'teste',
-    //   name: 'teste',
-    //   password: 'teste',
-    //   role: 'ADMIN',
-    //   confirmPassword: 'teste',
-    // });
 
     const sessionService = new CreateSessionsService();
     const { token } = await sessionService.execute({
-      email: 'testeee@email.com',
+      email: 'teste@email.com',
       password: 'teste',
-    })
+    });
 
     const updatedUserDetails: Partial<IUserDTO> = {
-      cellphone: '987654321',
-      lastname: 'Smith',
+      cellphone: '88888',
+      lastname: 'testou',
     };
+
     const response = await request('http://localhost:3333')
       .put(`/profile`)
       .send(updatedUserDetails)
       .set('Authorization', `bearer ${token}`);
 
-    //deleteId = user.id;
     expect(response.status).toBe(200);
 
-  
-
-
-    // const updatedUser = await prismaClient.user.findUnique({
-    //   where: { id: updatedUserDetails.id },
-    // });
-
-    // const expectedUser = { ...createUserResponse, ...updatedUserDetails };
-    // const userWithoutPassword = excludeFromObject(updatedUser, ['password']);
-
-    // expect(userWithoutPassword).toEqual(expectedUser);
+    expect(response.body.cellphone).toEqual(updatedUserDetails.cellphone);
+    expect(response.body.lastname).toEqual(updatedUserDetails.lastname);
   });
-});
+
+  });
