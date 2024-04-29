987f03045635a45799af90f5ba5d68a1d5c76163
Vitória Thaís <vitoriathdasilva@gmail.com>
2024-04-29 11:40:52 -0300

    add migrations

--- backend/src/prisma/generated/client/edge.js
+++ backend/src/prisma/generated/client/edge.js
@@ -165,7 +165,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- backend/src/prisma/generated/client/index.js
+++ backend/src/prisma/generated/client/index.js
@@ -166,7 +166,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
