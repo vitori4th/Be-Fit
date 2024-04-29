72e5a0e4565d05421ee256c841219cc5ad07c33f
Vitória Thaís <vitoriathdasilva@gmail.com>
2024-04-29 19:32:54 -0300

    add cypress teste

--- frontend/cypress/e2e/register/register.cy.js
+++ frontend/cypress/e2e/register/register.cy.js
@@ -13,5 +13,24 @@ describe('Teste Funcional de Cadastro', ()=>{
         cy.get(':nth-child(13) > .custom-input').type("TesteSenhaForte123@")
         cy.get('.bloco-cad > :nth-child(15)').type("TesteSenhaForte123@")
         cy.get('.bloco-cad > .button-login').click()
+        cy.get('.Toastify__toast-body > :nth-child(2)').should('contain', 'sucesso')
+    });
+})
+
+describe('Teste Funcional de Cadastro', ()=>{
+    it('Não deve cadastrar', ()=>{
+        cy.visit("http://localhost:3000/")
+        cy.get('.bg-transparent').click()
+        cy.get('.button-cad').click()
+        cy.get('.bloco-cad > :nth-child(3) > .custom-input').type("name_user")
+        cy.get('.bloco-cad > :nth-child(5) > .custom-input').type("valid_email@gmail.com")
+        cy.get(':nth-child(7) > .custom-input').type("2004-04-29")
+        cy.get(':nth-child(9) > .custom-input').type("123457")
+        cy.get(':nth-child(11) > .custom-input').type("12345678912")
+        cy.get(':nth-child(13) > .custom-input').type("TesteSenhaForte123@")
+        cy.get('.bloco-cad > :nth-child(15)').type("TesteSenhaForte123@")
+        cy.get('.bloco-cad > .button-login').click()
+        cy.get('.Toastify__toast-body > :nth-child(2)')
+        cy.get('.Toastify__toast-body > :nth-child(2)').should('contain', 'erro')
     });
 })
