f21f17edebc2431565c4056bd4e5fb386ff87613
Vitória Thaís <vitoriathdasilva@gmail.com>
2024-05-17 20:38:01 -0300

    add validation form

--- frontend/app/components/cadastro/cadastro.tsx
+++ frontend/app/components/cadastro/cadastro.tsx
@@ -3,93 +3,72 @@ import axios from 'axios';
 import Image from 'next/image';
 import mail from '../../../public/icons/mail.png';
 import pass from '../../../public/icons/pass.png';
-import visible from '../../../public/icons/visible.png';
 import './cadastro.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 
 interface CadastroModalProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
+const createUserFormSchema = z.object({
+  name: z.string()
+    .nonempty("Username required")
+    .regex(/^[A-Za-z]+$/i, "Only letters allowed")
+    .max(50, "Name max 50 characters"),
+  lastname: z.string()
+    .nonempty("Username required")
+    .regex(/^[A-Za-z]+$/i, "Only letters allowed")
+    .max(50, "Name max 50 characters"),
+  email: z.string().email(),
+  dateBirth: z.string()
+    .regex(/^\d{4}-\d{2}-\d{2}$/, "Invalid Date format"),
+  cellphone: z.string()
+    .regex(/^\d{11}$/, "Phone must have 11 digits"),
+  cpf: z.string()
+    .regex(/^\d{11}$/, "CPF must have 11 digits"),
+  password: z.string()
+    .min(8, "Password min 8 characters")
+    .regex(/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/, "Password requires uppercase, number, special char")
+    .nonempty("Password required"),
+  confirmPassword: z.string()
+    .nonempty("Confirm password required")
+})
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"]
+  });
+
+type CreateUserFormSchema = z.infer<typeof createUserFormSchema>
+
 const CadastroModal = ({ isOpen, onClose }: CadastroModalProps) => {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [dataNascimento, setDataNascimento] = useState('');
-  const [cpf, setCpf] = useState('');
-  const [telefone, setTelefone] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const { register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<CreateUserFormSchema>({
+    resolver: zodResolver(createUserFormSchema)
+  });
+
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  const notify = () => toast.success("Usuário Cadastrado com Sucesso!");
-
-  const handleNomeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNome(event.target.value);
-  };
-
-  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(event.target.value);
-  };
-
-  const handleDataNascimentoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDataNascimento(event.target.value);
-  };
-
-  const handleCpfChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCpf(event.target.value);
-  };
-
-  const handleTelefoneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTelefone(event.target.value);
-  };
-
-  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value);
-  };
-
-  const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setConfirmPassword(event.target.value);
-  };
-
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword(!showConfirmPassword);
-  };
-
-  const successLogin = () => {
-    toast.success('Usuário cadastrado com sucesso!');
-  };
-
-  const handleCadastroClick = async () => {
-    if (nome.trim() === '' || email.trim() === '' || dataNascimento.trim() === '' || cpf.trim() === '' || telefone.trim() === '' || password.trim() === '' || confirmPassword.trim() === '') {
-      toast.error("Preencha todos os campos");
-      return;
-    }
-
-    if (password !== confirmPassword) {
-      toast.error('As senhas não coincidem. Por favor, verifique.');
-      return;
-    }
-
+  const handleCadastroClick = async (data: CreateUserFormSchema) => {
     try {
       const userData = {
-        cellphone: telefone,
-        cpf: Number(cpf),
-        email,
-        dateBirth: new Date(dataNascimento),
-        lastname: 'Sobrenome',
-        name: nome,
-        password,
-        confirmPassword,
+        name: data.name,
+        lastname: data.lastname,
+        cellphone: data.cellphone,
+        cpf: data.cpf,
+        email: data.email,
+        dateBirth: data.dateBirth,
+        password: data.password,
+        confirmPassword: data.confirmPassword,
       };
-      
+
       console.log(userData);
       // Chamar a API para o registro do usuário
       const response = await axios.post('http://localhost:3333/users/', userData);
@@ -113,60 +92,81 @@ const CadastroModal = ({ isOpen, onClose }: CadastroModalProps) => {
   return (
     <div className={`modal-overlay ${isOpen ? 'active' : ''}`} onClick={onClose}>
       <div className="modal-content delay-[2000ms]" onClick={(e) => e.stopPropagation()}>
-        <div className="bloco-cad mb-[-20%] flex flex-col justify-center items-center">
-          <h2 className="title-cad font-bold mt-[-30%]">Cadastro</h2>
-          <p className="sub-info mt-8 mb-2">Nome</p>
-          <div className="custom-input-container">
-            <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
-            <input type="text" className="custom-input flex items-center" placeholder="Nome" value={nome} onChange={handleNomeChange} />
-          </div>
-          <p className="sub-info mt-2 mb-2">Email</p>
-          <div className="custom-input-container">
-            <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
-            <input type="text" className="custom-input flex items-center" placeholder="Email" value={email} onChange={handleEmailChange} />
-          </div>
-          <p className="sub-info mt-2 mb-2">Data de Nascimento</p>
-          <div className="custom-input-container">
-            <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
-            <input type="date" className="custom-input flex items-center" placeholder="Data de Nascimento" value={dataNascimento} onChange={handleDataNascimentoChange} />
-          </div>
-          <p className="sub-info mt-2 mb-2">CPF</p>
-          <div className="custom-input-container">
-            <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
-            <input type="text" className="custom-input flex items-center" placeholder="CPF" value={cpf} onChange={handleCpfChange} />
-          </div>
-          <p className="sub-info mt-2 mb-2">Telefone</p>
-          <div className="custom-input-container">
-            <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
-            <input type="text" className="custom-input flex items-center" placeholder="Telefone" value={telefone} onChange={handleTelefoneChange} />
-          </div>
-          <p className="sub-info mt-2 mb-2">Senha</p>
-      <div className="custom-input-container">
-        <Image src={pass} alt="Icone" width={13.33} height={10.67} className="input-icon" />
-        <input
-          type={showPassword ? 'text' : 'password'}
-          className="custom-input"
-          placeholder="Senha"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-       
-      </div>
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="title-cad font-bold">Cadastro</h2>
+          <form onSubmit={handleSubmit(handleCadastroClick)}>
+            <p className="sub-info mb-2">Nome</p>
+            <div className="custom-input-container">
+              <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
+              <input type="text" className="custom-input flex items-center" placeholder="Nome" {...register('name')} />
+            </div>
+            {errors.name && (<span className='sub-info w-full flex justify-start text-red-700'>{errors.name?.message}</span>)}
+            <p className="sub-info mb-2">Sobrenome</p>
+            <div className="custom-input-container">
+              <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
+              <input type="text" className="custom-input flex items-center" placeholder="Nome" {...register('lastname')} />
+            </div>
+            {errors.lastname && (<span className='sub-info w-full flex justify-start text-red-700'>{errors.lastname?.message}</span>)}
 
-      <p className="sub-info mt-2 mb-2">Confirmar Senha</p>
-      <div className="custom-input-container">
-        <Image src={pass} alt="Icone" width={13.33} height={10.67} className="input-icon" />
-        <input
-          type={showConfirmPassword ? 'text' : 'password'}
-          className="custom-input"
-          placeholder="Confirmar Senha"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-        />
-        
-      </div>
-          <button className="button-login border border-green-800 rounded-md duration-500 mt-10 hover:border-green-600 hover:text-green-600" onClick={handleCadastroClick}>Cadastrar</button>
-          <ToastContainer />
+            <p className="sub-info mt-2 mb-2">Email</p>
+            <div className="custom-input-container">
+              <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
+              <input type="text" className="custom-input flex items-center" placeholder="Email"  {...register('email')} />
+            </div>
+            {errors.email && (<span className='sub-info w-full flex justify-start text-red-700'>{errors.email?.message}</span>)}
+
+            <p className="sub-info mt-2 mb-2">Data de Nascimento</p>
+            <div className="custom-input-container">
+              <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
+              <input type="date" className="custom-input flex items-center" placeholder="Data de Nascimento"  {...register('dateBirth')} />
+            </div>
+            {errors.dateBirth && (<span className='sub-info w-full flex justify-start text-red-700'>{errors.dateBirth?.message}</span>)}
+
+            <p className="sub-info mt-2 mb-2">CPF</p>
+            <div className="custom-input-container">
+              <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
+              <input type="text" className="custom-input flex items-center" placeholder="CPF" {...register('cpf')} />
+            </div>
+            {errors.cpf && (<span className='sub-info w-full flex justify-start text-red-700'>{errors.cpf?.message}</span>)}
+
+            <p className="sub-info mt-2 mb-2">Telefone</p>
+            <div className="custom-input-container">
+              <Image src={mail} alt="Icone" width={13.33} height={10.67} className="input-icon" />
+              <input type="text" className="sub-info custom-input flex items-center" placeholder="Telefone" {...register('cellphone')} />
+            </div>
+            {errors.cellphone && (<span className='w-full flex justify-start text-red-700'>{errors.cellphone?.message}</span>)}
+
+            <p className="sub-info mt-2 mb-2">Senha</p>
+            <div className="custom-input-container">
+              <Image src={pass} alt="Icone" width={13.33} height={10.67} className="input-icon" />
+              <input
+                type={showPassword ? 'text' : 'password'}
+                {...register('password')}
+                className="custom-input"
+                placeholder="Senha"
+              />
+            </div>
+            {errors.password && (<span className=' sub-info w-full flex justify-start text-red-700'>{errors.password?.message}</span>)}
+
+
+            <p className="sub-info mt-2 mb-2">Confirmar Senha</p>
+            <div className="custom-input-container">
+              <Image src={pass} alt="Icone" width={13.33} height={10.67} className="input-icon" />
+              <input
+                type={showConfirmPassword ? 'text' : 'password'}
+                {...register('confirmPassword')}
+                className="custom-input"
+                placeholder="Confirmar Senha"
+              />
+            </div>
+            {errors.confirmPassword && (<span className='sub-info w-full flex justify-start text-red-700'>{errors.confirmPassword?.message}</span>)}
+
+            <button className="button-login border border-green-800 rounded-md duration-500 mt-5 hover:border-green-600 hover:text-green-600"
+              disabled={isSubmitting}
+              type='submit'
+            >{isSubmitting ? 'Cadastrando...' : 'Cadastrar'}</button>
+          </form>
+          <ToastContainer containerId={"friendRequest"}/>
           <button className="button-return rounded-md hover:bg-gray-400 duration-500 mt-2" onClick={onClose}>Voltar</button>
         </div>
       </div>
